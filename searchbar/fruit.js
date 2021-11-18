a836b3c04b0f892fc21ec19176ed49bebeeb55d7
Jyoshna-2021 <91448597+Jyoshna-2021@users.noreply.github.com>
2021-11-18 12:28:24 +0530

    Update fruit.js

--- searchbar/fruit.js
+++ searchbar/fruit.js
@@ -8,46 +8,55 @@ function search_fruits(e) {
 
 const listOffruit = [
   {
+    id:"1",
     name: "Apple",
     price: "80",
     url: "images/apple-juice.jpg"
   },
   {
+    id:"2",
     name: "Mango",
     price: "90",
     url: "images/mango-juice.jpg"
   },
   {
+    id:"3",
     name: "Orange",
     price: "70",
     url: "images/orange.jpeg"
   },
   {
+    id:"4",
     name: "banana",
     price: "30",
     url: "images/banana.jpg"
   },
   {
+    id:"5",
     name: "blueberry",
     price: "40",
     url: "images/blueberry.jpg"
   },
   {
+    id:"6",
     name: "grape",
     price: "50",
     url: "images/grape.jpg"
   },
   {
+    id:"7",
     name: "kiwi",
     price: "80",
     url: "images/kiwi.jpeg"
   },
   {
+    id:"8",
     name: "watermelon",
     price: "30",
     url: "images/watermelon.jpg"
   },
   {
+    id:"9",
     name: "pomegranate",
     price: "45",
     url: "images/pomegranate.jpg"
@@ -77,3 +86,4 @@ function writeData(items) {
 initialStore();
 const searchInput = document.querySelector("#searchbar");
 searchInput.addEventListener("keyup", search_fruits);
+
